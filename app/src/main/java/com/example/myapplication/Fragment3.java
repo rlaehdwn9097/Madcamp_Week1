ba8a84c263f248f8a4472d86be83b56303b50bc3
rlaehdwn9097 <rlaehdwn9097@gmail.com>
2021-01-02 21:57:21 +0900

    여러개 ployLine 추가.

--- app/src/main/java/com/example/myapplication/Fragment3.java
+++ app/src/main/java/com/example/myapplication/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class Fragment3 extends Fragment implements OnMapReadyCallback {
     GoogleMap mMap;
@@ -38,13 +43,18 @@ public class Fragment3 extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         //지도타입 - 일반
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        oneMarker();
+        //oneMarker();
         //manyMarker();
+        //drawPloyLines();
+        drawMultiPloyLines();
     }
+
     //마커하나찍는 기본 예제
     public void oneMarker() {
         // 서울 여의도에 대한 위치 설정
         LatLng seoul = new LatLng(37.52487, 126.92723);
+        LatLng kaist = new LatLng(36.221920,  127.214680);
+
 
         // 구글 맵에 표시할 마커에 대한 옵션 설정  (알파는 좌표의 투명도이다.)
         MarkerOptions makerOptions = new MarkerOptions();
@@ -104,6 +114,143 @@ public class Fragment3 extends Fragment implements OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.52487, 126.92723)));
     }
 
+    public void drawPloyLines() {
+        // 서울 여의도에 대한 위치 설정
+        LatLng seoul = new LatLng(37.52487, 126.92723);
+        LatLng kaist = new LatLng(36.372147,127.3582013);
+
+
+        MarkerOptions makerOptions = new MarkerOptions();
+
+        // 구글 맵에 표시할 마커에 대한 옵션 설정  (알파는 좌표의 투명도이다.)
+
+        makerOptions
+                .position(seoul)
+                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.")
+                .snippet("여기는 여의도인거같네여!!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .alpha(0.5f);
+
+        // 마커를 생성한다. showInfoWindow를 쓰면 처음부터 마커에 상세정보가 뜨게한다. (안쓰면 마커눌러야뜸)
+        mMap.addMarker(makerOptions); //.showInfoWindow();
+
+        makerOptions
+                .position(kaist)
+                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.")
+                .snippet("여기는 카이스트인거같네여!!")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .alpha(0.5f);
+
+        // 마커를 생성한다. showInfoWindow를 쓰면 처음부터 마커에 상세정보가 뜨게한다. (안쓰면 마커눌러야뜸)
+        mMap.addMarker(makerOptions); //.showInfoWindow();
+
+
+        //정보창 클릭 리스너
+        mMap.setOnInfoWindowClickListener(infoWindowClickListener);
+
+        //마커 클릭 리스너
+        mMap.setOnMarkerClickListener(markerClickListener);
+
+        //카메라를 여의도 위치로 옮긴다.
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+        //처음 줌 레벨 설정 (해당좌표=>서울, 줌레벨(16)을 매개변수로 넣으면 된다.) (위에 코드대신 사용가능)(중첩되면 이걸 우선시하는듯)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 16));
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(seoul, kaist)
+                .width(5)
+                .color(Color.RED));
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getContext(), "눌렀습니다!!", Toast.LENGTH_LONG);
+                return false;
+            }
+        });
+
+    }
+
+    public void drawMultiPloyLines() {
+        // 서울 여의도에 대한 위치 설정
+        LatLng seoul = new LatLng(37.52487, 126.92723);
+        LatLng kaist = new LatLng(36.372147,127.3582013);
+        LatLng beijing = new LatLng(39.9390731,116.1172792);
+        LatLng moscow = new LatLng(55.5815245,36.8251409);
+        LatLng praha = new LatLng(50.0598058,14.3255421);
+        LatLng berlin = new LatLng(52.5069704,13.2846503);
+        LatLng paris = new LatLng(48.8589507,2.2770205);
+        LatLng london = new LatLng(51.5287718,-0.2416802);
+        LatLng newyork = new LatLng(40.6976637,-74.1197637);
+        LatLng losangeles = new LatLng(34.0207305,-118.6919144);
+        LatLng tokyo = new LatLng(35.6684415,139.6007845);
+
+        ArrayList<LatLng> country = new ArrayList<>();
+        country.add(kaist);
+        country.add(seoul);
+        country.add(beijing);
+        country.add(moscow);
+        country.add(praha);
+        country.add(berlin);
+        country.add(paris);
+        country.add(london);
+        country.add(newyork);
+        country.add(losangeles);
+        country.add(tokyo);
+
+        MarkerOptions makerOptions = new MarkerOptions();
+
+        for(int i=0; i < country.size(); i++){
+            makerOptions
+                    .position(country.get(i))
+                    .title("마커" + i);
+                    /*
+                    .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .alpha(0.5f);
+
+             */
+
+            mMap.addMarker(makerOptions);
+        }
+
+
+        //정보창 클릭 리스너
+        mMap.setOnInfoWindowClickListener(infoWindowClickListener);
+
+        //마커 클릭 리스너
+        mMap.setOnMarkerClickListener(markerClickListener);
+
+        //카메라를 여의도 위치로 옮긴다.
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+        //처음 줌 레벨 설정 (해당좌표=>서울, 줌레벨(16)을 매개변수로 넣으면 된다.) (위에 코드대신 사용가능)(중첩되면 이걸 우선시하는듯)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kaist, 16));
+
+        for (int i = 0; i < country.size() - 1; i++) {
+            LatLng src = country.get(i);
+            LatLng dest = country.get(i + 1);
+
+            // mMap is the Map Object
+            Polyline line = mMap.addPolyline(
+                    new PolylineOptions().add(
+                            new LatLng(src.latitude, src.longitude),
+                            new LatLng(dest.latitude,dest.longitude)
+                    ).width(5).color(Color.BLUE).geodesic(true)
+            );
+        }
+/*
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getContext(), "눌렀습니다!!", Toast.LENGTH_LONG);
+                return false;
+            }
+        });
+*/
+
+
+    }
+
     //마커정보창 클릭리스너는 다작동하나, 마커클릭리스너는 snippet정보가 있으면 중복되어 이벤트처리가 안되는거같다.
     // oneMarker(); 는 동작하지않으나 manyMarker(); 는 snippet정보가 없어 동작이가능하다.
 
@@ -123,6 +270,7 @@ public class Fragment3 extends Fragment implements OnMapReadyCallback {
             String markerId = marker.getId();
             //선택한 타겟위치
             LatLng location = marker.getPosition();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,10));
             Toast.makeText(getContext(), "마커 클릭 Marker ID : "+markerId+"("+location.latitude+" "+location.longitude+")", Toast.LENGTH_SHORT).show();
             return false;
         }
