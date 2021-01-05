a3144de34006d15cadf08835347d1e8822dcae0f
wkdgywls <sunshiny.best@gmail.com>
2021-01-05 17:13:37 +0900

    gallery add

---
+++ app/src/main/java/com/example/myapplication/Imagecontent.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class image {
+
+    public String image;
+
+    public image(String image){
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
